6b7622170fc3c1208907ce0bea57bfe34e63b8d8
Bakhta Elamar <elamarb@icloud.com>
2018-10-29 05:14:26 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
 def hello_t
-
+["Tim", "Tom", "Jim"].each do |name|
+  puts "Hi, #{name}"
 end
 
 # call your method here!
 
+["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
+def yielding
+  puts "the program is executing the code inside the method"
+  yield
+  puts "now we are back in the method"
+end
